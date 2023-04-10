8262fa59154a9e52285bab9d37840593fc1980dc
tennisha <tennisha@gmail.com>
2023-04-09 23:56:32 -0400

    Create Deck.py

---
+++ Deck.py
@@ -0,0 +1,20 @@
+#!/bin/bash/python3
+from cards import Cards
+class Deck:
+    
+        def __init__ (self):
+            self.cards = []
+            self.build()
+            self.showcards()
+            
+        def build (self):
+            for s in ['spade','heart','club','diamond']:
+                for v in ['ace','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king']:
+                    self.cards.append (Cards(s,v))
+                    #print (Cards(s,v).show_card)
+                
+        def showcards (self):
+            for c in self.cards: 
+                c.show_card()
+                
+                
