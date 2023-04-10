d879cf182a6d227fe50a4716cb6a88e9bc314354
tennisha <tennisha@gmail.com>
2023-04-09 23:16:37 -0400

    Update Cards.py

--- Cards.py
+++ Cards.py
@@ -6,10 +6,21 @@ class Cards:
         
     
     def check_suit(self,suit):
-        suit_types = ['Spade','Heart','Club','Diamond']
+        suit_types = ['spade','heart','club','diamond']
         print (self.suit)
-        if (self.suit in suit_types):
+        if (self.suit.lower() in suit_types):
           return True
         else:
             return False
           
+ 
+    def check_value (self,value):
+            value_types = ['ace','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king']
+            print (self.value)
+            if (self.value.lower() in value_types):
+                return True
+            else: 
+                print ('You have entered an incorrect value, Please spell out the card Value. For example a 2 is Two.')
+                return False
+                
+                
